4d71078a21c47498762a9ba9327f7f55f2205fc6
Zim Do <zimd0110@gmail.com>
2025-07-08 23:31:45 +1000

    testing endpoints/routes

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,21 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-    res.status(201).send("You have 10 notes");
-})
+  res.status(200).send("You have 10 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "Post created successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Post updated successfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Post updated successfully!" });
+});
 
 app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
-})
+  console.log("Server started on PORT: 5001");
+});
