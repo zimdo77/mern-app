c1676922c824f214a8e640eb7170a4ea2e7c492e
Zim Do <zimd0110@gmail.com>
2025-08-01 17:00:44 +1000

    setup frontend with Vite + React

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route } from "react-router";
+import { Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+
+const App = () => {
+  return (
+    <div data-theme="forest">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
